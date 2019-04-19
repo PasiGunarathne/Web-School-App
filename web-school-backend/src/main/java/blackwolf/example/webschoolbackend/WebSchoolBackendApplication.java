b6a305526d581f1135b5fe42a259b0a639a95e4e
PasiGunarathne <pasigunarathne@gmail.com>
2019-04-19 19:30:16 +0530

    first commit

---
+++ web-school-backend/src/main/java/blackwolf/example/webschoolbackend/WebSchoolBackendApplication.java
@@ -0,0 +1,13 @@
+package blackwolf.example.webschoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSchoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSchoolBackendApplication.class, args);
+	}
+
+}
